73f42ede77a719c3db106896a2eca01bff4f0df7
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-22 12:14:19 +0900

    add Favorite Model

--- server/models/Favorite.js
+++ server/models/Favorite.js
@@ -1,14 +1,26 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
-
-const userSchema = mongoose.Schema({
+const favoriteSchema = mongoose.Schema({
     userFrom: {
         type: Schema.Types.ObjectId,
-        ref: 'User'
+        ref: 'User' // ObjectId를 type으로 할시 ref를 선언해주어야 하는데, 예를 들어 userId가 sdfadsfbhkasb23이라고 한다면, 이것을 가지고 models의 User.js안의 모든 정보를 가지고 올 수있다.
+    },
+    movieId: {
+        type: String
+    },
+    movieTitle: {
+        type: String
+    },
+    moviePost: {
+        type: String
+    },
+    movieRunTime: {
+        type: String
     }
-})
+}, {timestamps: true }) // timestamps: true를 해주면 생성된 시간등을 자동으로 처리해줌
 
 
-const User = mongoose.model('User', userSchema);
+const Favorite = mongoose.model('Favorite', favoriteSchema);
 
-module.exports = { User }
+module.exports = { Favorite }
