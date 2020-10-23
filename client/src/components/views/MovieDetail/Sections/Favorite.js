783a6ea57d9b8192ead037b620e281954bc96a19
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-23 12:27:20 +0900

    added Favorited

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,5 @@
 import Axios from 'axios'
-import React, {useEffect} from 'react'
+import React, {useEffect, useState } from 'react'
 
 
 function Favorite(props) {
@@ -10,6 +10,10 @@ function Favorite(props) {
     const moviePost = props.movieInfo.backdrop_path
     const movieRunTime = props.movieInfo.runtime
 
+
+    const [FavoriteNumber, setFavoriteNumber] = useState(0);
+    const [Favorited, setFavorited] = useState(false);
+
 useEffect(() => {
     
     let variables = {
@@ -18,19 +22,27 @@ useEffect(() => {
     }
     Axios.post('/api/favorite/favoriteNumber', variables)
         .then(response => {
-            console.log("response.data", response.data);
             if(response.data.success){
-                
+                setFavoriteNumber(response.data.favoriteNumber)
             } else {
                 alert('숫자 정보를 가져오는데 실패 했습니다.')
             }
         })
 
+    Axios.post('/api/favorite/facorited', variables)
+        .then(response => {
+            if(response.data.success){
+                setFavorited(response.data.favorited)
+            } else {
+                alert('정보를 가져오는데 실패 했습니다.')
+            }
+        })    
+
 }, [])
 
     return (
         <div>
-            <button>Favorite</button>
+            <button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</button>
         </div>
     )
 }
--- server/routes/Favorite.js
+++ server/routes/Favorite.js
@@ -11,16 +11,29 @@ router.post('/favoriteNumber', (req, res) => {
     Favorite.find({ "movieId": req.body.movieId })
         .exec(( err, info) => {
         if(err) return res.status(400).send(err)
-
-
         // 그다음에 프론트에 다시 숫자 정보를 보내주기
         res.status(200).json({ success: true, favoriteNumber: info.length })
     })
-
-    
-
-
 })
+
+router.post('/favorited', (req, res) => {
+
+    // 내가 이 영화를 Favorite 리스트에 넣었는지 정보를 DB에서 가져오기
+
+    // 
+    Favorite.find({ "movieId": req.body.movieId, "userFrom": req.body.userFrom })
+        .exec(( err, info) => {
+        if(err) return res.status(400).send(err)
+        // 그다음에 프론트에 다시 숫자 정보를 보내주기
+        let result = false;
+        if (info.length !== 0) {
+            result = true
+        }
+
+        res.status(200).json({ success: true, favoriteNumber: info.length })
+    })
+})
+
 
 
 module.exports = router;
