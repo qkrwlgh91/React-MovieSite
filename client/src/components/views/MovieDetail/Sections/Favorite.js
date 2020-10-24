fd1926cba7e704e33182d355e074267410fec2b7
qkrwlgh91 <qkrwlgh91@gmail.com>
2020-10-24 14:15:46 +0900

    added favorite function ( + or -)

--- client/src/components/views/MovieDetail/Sections/Favorite.js
+++ client/src/components/views/MovieDetail/Sections/Favorite.js
@@ -1,5 +1,6 @@
 import Axios from 'axios'
 import React, {useEffect, useState } from 'react'
+import { Button } from 'antd';
 
 
 function Favorite(props) {
@@ -14,12 +15,17 @@ function Favorite(props) {
     const [FavoriteNumber, setFavoriteNumber] = useState(0);
     const [Favorited, setFavorited] = useState(false);
 
-useEffect(() => {
-    
     let variables = {
         userFrom,
-        movieId
+        movieId,
+        movieTitle,
+        moviePost,
+        movieRunTime
     }
+
+useEffect(() => {
+    
+    
     Axios.post('/api/favorite/favoriteNumber', variables)
         .then(response => {
             if(response.data.success){
@@ -40,9 +46,34 @@ useEffect(() => {
 
 }, [])
 
+const onClickFavorite = () => {
+
+    if (Favorited) {
+        Axios.post('/api/favorite/removeFromFavorite', variables)
+            .then(response => {
+                if(response.data.success) {
+                    setFavoriteNumber(FavoriteNumber - 1)
+                    setFavorited(!Favorited)
+                } else {
+                    alert('Favorite 리스트에서 지우는 걸 실패했습니다.')
+                }
+            })
+    } else {
+        Axios.post('/api/favorite/addToFavorite', variables)
+            .then(response => {
+                if(response.data.success) {
+                    setFavoriteNumber(FavoriteNumber + 1)
+                    setFavorited(!Favorited)
+                } else {
+                    alert('Favorite 리스트에서 추가하는 걸 실패했습니다.')
+                }
+            })
+    }
+}
+
     return (
         <div>
-            <button>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</button>
+            <Button onClick={onClickFavorite}>{Favorited ? "Not Favorite" : "Add to Favorite"} {FavoriteNumber}</Button>
         </div>
     )
 }
--- server/routes/Favorite.js
+++ server/routes/Favorite.js
@@ -34,6 +34,25 @@ router.post('/favorited', (req, res) => {
     })
 })
 
+router.post('/removeFromFavorite', (req, res) => {
 
+    Favorite.findOneAndDelete({ movieId: req.body.movieId , userFrom: req.body.userFrom })
+        .exec(( err, doc ) => {
+            if(err) return res.status(400).send(err)
+            res.status(200).json({ success: true, doc })
+        })
+
+})
+
+router.post('/addToFavorite', (req, res) => {
+
+    const favorite = new Favorite(req.body);
+
+    favorite.save((err, doc) => {
+        if(err) return res.status(400).send(err)
+        return res.status(200).json({ success: true })
+    })
+
+})
 
 module.exports = router;
